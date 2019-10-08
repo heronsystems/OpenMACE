128f0936e715bc183e3f088a2b70e18356792f4b
majorpr13 <kkroeger@araustech.com>
2019-10-08 16:04:10 -0400

    Comment Out Plotting Matlab

--- MATLAB/ROS/positionCallback.m
+++ MATLAB/ROS/positionCallback.m
@@ -5,7 +5,7 @@ function [ output_args ] = positionCallback( subscriber, msg )
      X = sprintf('UPDATE_POSITION: %f %f %f',msg.Northing,msg.Easting,msg.Altitude);
      disp(X);
 
-     plot(msg.Northing, msg.Easting,'r*')
-     hold on
+%      plot(msg.Northing, msg.Easting,'r*')
+%      hold on
 end
 
