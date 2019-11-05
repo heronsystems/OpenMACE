14d3d86beb2f0340b8acb842119713b0ace8e7fd
Sheng <wesleycheng.umd@gmail.com>
2019-11-05 16:17:33 -0500

    post-flight commit

--- MATLAB/UMD_F3/F3CheckoutFlight.m
+++ MATLAB/UMD_F3/F3CheckoutFlight.m
@@ -28,61 +28,11 @@ tStart = tic;
 
 
 % % ============= Test 1: N Quads Takeoff, Wpt Mission, and Land ==============
-% ROS_MACE.N = 4;
-% %ROS_MACE.operationalAlt = [4 8]; % m
-% %ROS_MACE.agentIDs = [1 2]; % m
-% ROS_MACE.operationalAlt = [2 3 4 5]; % m
-% ROS_MACE.agentIDs = [3 4 5 6]; % SYSID_THISMAV on each quadrotor
-% 
-% ROS_MACE.agentIDtoIndex = zeros(1,max(ROS_MACE.agentIDs));
-% ROS_MACE.wptCoordinator = 'integrated';
-% 
-% for i = 1:1:length(ROS_MACE.agentIDs)
-%     ROS_MACE.agentIDtoIndex( ROS_MACE.agentIDs(i) ) = i;
-% end
-% 
-% ROS_MACE = launchROS( ROS_MACE );
-% swarmState = sendDatumAndWaitForGPS( ROS_MACE );
-% armAndTakeoff( ROS_MACE );
-% disp('Press any key to launch waypoint mission...')
-% pause;
-% 
-% % evenly distribute N quads between y from 1m to 11m and -11m to -1m
-% temp = linspace(0,20,ROS_MACE.N+2);
-% temp = temp(2:end-1);
-% yLocation = temp(temp<10)-11;
-% yLocation = [yLocation temp(temp>=10)-9];
-% 
-% wpts = cell(1,ROS_MACE.N);
-% 
-% for k = 1:ROS_MACE.N
-%     if yLocation(k)>0
-%         wpts{k} = [11-yLocation(k) yLocation(k);...
-%                    -9-yLocation(k) yLocation(k);...
-%                    11-yLocation(k) yLocation(k)];
-%     else
-%         wpts{k} = [11+yLocation(k) yLocation(k);...
-%                     -9+yLocation(k) yLocation(k);...
-%                    11+yLocation(k) yLocation(k)];
-%     end
-% end
-% % wpts{1} = [5 6;-15 6;10 6;]; % each vector is for a single agent
-% % wpts{1} = [5 -6;-15 -6;10 -6];
-% 
-% 
-% captureRadius = 1;% 1.2;
-% wptManager( ROS_MACE, wpts, captureRadius);
-% 
-% disp('Press any key to land...')
-% pause;
-% land( ROS_MACE );
-
-% % ============= Test 2: 1 Quads Takeoff, ascending waypoints, and Land ==============
-ROS_MACE.N = 1;
+ROS_MACE.N = 4;
 %ROS_MACE.operationalAlt = [4 8]; % m
 %ROS_MACE.agentIDs = [1 2]; % m
-ROS_MACE.operationalAlt = [2]; % m
-ROS_MACE.agentIDs = [3]; % SYSID_THISMAV on each quadrotor
+ROS_MACE.operationalAlt = [2 3 2 3]; % m
+ROS_MACE.agentIDs = [3 4 5 6]; % SYSID_THISMAV on each quadrotor
 
 ROS_MACE.agentIDtoIndex = zeros(1,max(ROS_MACE.agentIDs));
 ROS_MACE.wptCoordinator = 'integrated';
@@ -105,50 +55,100 @@ yLocation = [yLocation temp(temp>=10)-9];
 
 wpts = cell(1,ROS_MACE.N);
 
-wpts{1} = [10 -5]; % push the quad to some place near the shed
+for k = 1:ROS_MACE.N
+    if yLocation(k)>0
+        wpts{k} = [11-yLocation(k) yLocation(k);...
+                   1-yLocation(k) yLocation(k);...
+                   11-yLocation(k) yLocation(k)];
+    else
+        wpts{k} = [11+yLocation(k) yLocation(k);...
+                   1+yLocation(k) yLocation(k);...
+                   11+yLocation(k) yLocation(k)];
+    end
+end
+% wpts{1} = [5 6;-15 6;10 6;]; % each vector is for a single agent
+% wpts{1} = [5 -6;-15 -6;10 -6];
+
 
 captureRadius = 1;% 1.2;
 wptManager( ROS_MACE, wpts, captureRadius);
-
-altitudeSequence = [ROS_MACE.operationalAlt(1) 2.5 3 3.5  4 4.5 4.7 4.9 5 5.1 5.2];
-
-for k = 1:length(altitudeSequence)
-    waypointRequest = rosmessage(ROS_MACE.waypointClient);
-    waypointRequest.Timestamp = rostime('now');
-    waypointRequest.VehicleID = ROS_MACE.agentIDs(i);   % Vehicle ID
-    waypointRequest.CommandID = 3; % TODO: Set command ID enum in MACE
-    switch ROS_MACE.coordSys
-        case 'ENU'
-            waypointRequest.Easting = wpts{1}(1,1); % Relative easting position to Datum
-            waypointRequest.Northing = wpts{1}(1,2); % Relative northing position to Datum
-        case 'F3'
-            [east, north] = F3toENU(wpts{1}(1,1), wpts{1}(1,2));
-            waypointRequest.Easting = east; % Relative easting position to Datum
-            waypointRequest.Northing = north; % Relative northing position to Datum
-    end
-    waypointRequest.Altitude = altitudeSequence(k);
-    fprintf('Commanding quad to alt = %f.\n',altitudeSequence(k));
-    waypointResponse = call(ROS_MACE.waypointClient, waypointRequest, 'Timeout', 10);
-    
-    % pausing for 10 seconds (total will be about 220 s)
-    for j = 1:20
-        msgGeo = ROS_MACE.geopositionSub.LatestMessage;
-        pause(0.5);
-        positionCallback(ROS_MACE,msgGeo);
-    end
-end
 
 disp('Press any key to land...')
 pause;
 land( ROS_MACE );
 
-% ============= Test: 3 quad takeoff, kinematic command, and land =========
-
+% % ============= Test 2: 1 Quads Takeoff, ascending waypoints, and Land ==============
 % ROS_MACE.N = 1;
 % %ROS_MACE.operationalAlt = [4 8]; % m
 % %ROS_MACE.agentIDs = [1 2]; % m
 % ROS_MACE.operationalAlt = [2]; % m
-% ROS_MACE.agentIDs = [1]; % SYSID_THISMAV on each quadrotor
+% ROS_MACE.agentIDs = [3]; % SYSID_THISMAV on each quadrotor
+% 
+% ROS_MACE.agentIDtoIndex = zeros(1,max(ROS_MACE.agentIDs));
+% ROS_MACE.wptCoordinator = 'integrated';
+% 
+% for i = 1:1:length(ROS_MACE.agentIDs)
+%     ROS_MACE.agentIDtoIndex( ROS_MACE.agentIDs(i) ) = i;
+% end
+% 
+% ROS_MACE = launchROS( ROS_MACE );
+% swarmState = sendDatumAndWaitForGPS( ROS_MACE );
+% armAndTakeoff( ROS_MACE );
+% disp('Press any key to launch waypoint mission...')
+% pause;
+% 
+% % evenly distribute N quads between y from 1m to 11m and -11m to -1m
+% temp = linspace(0,20,ROS_MACE.N+2);
+% temp = temp(2:end-1);
+% yLocation = temp(temp<10)-11;
+% yLocation = [yLocation temp(temp>=10)-9];
+% 
+% wpts = cell(1,ROS_MACE.N);
+% 
+% wpts{1} = [10 -5]; % push the quad to some place near the shed
+% 
+% captureRadius = 1;% 1.2;
+% wptManager( ROS_MACE, wpts, captureRadius);
+% 
+% altitudeSequence = [ROS_MACE.operationalAlt(1) 2.5 3 3.5  4 4.5 4.7 4.9 5 5.1 5.2];
+% 
+% for k = 1:length(altitudeSequence)
+%     waypointRequest = rosmessage(ROS_MACE.waypointClient);
+%     waypointRequest.Timestamp = rostime('now');
+%     waypointRequest.VehicleID = ROS_MACE.agentIDs(i);   % Vehicle ID
+%     waypointRequest.CommandID = 3; % TODO: Set command ID enum in MACE
+%     switch ROS_MACE.coordSys
+%         case 'ENU'
+%             waypointRequest.Easting = wpts{1}(1,1); % Relative easting position to Datum
+%             waypointRequest.Northing = wpts{1}(1,2); % Relative northing position to Datum
+%         case 'F3'
+%             [east, north] = F3toENU(wpts{1}(1,1), wpts{1}(1,2));
+%             waypointRequest.Easting = east; % Relative easting position to Datum
+%             waypointRequest.Northing = north; % Relative northing position to Datum
+%     end
+%     waypointRequest.Altitude = altitudeSequence(k);
+%     fprintf('Commanding quad to alt = %f.\n',altitudeSequence(k));
+%     waypointResponse = call(ROS_MACE.waypointClient, waypointRequest, 'Timeout', 10);
+%     
+%     % pausing for 10 seconds (total will be about 220 s)
+%     for j = 1:20
+%         msgGeo = ROS_MACE.geopositionSub.LatestMessage;
+%         pause(0.5);
+%         positionCallback(ROS_MACE,msgGeo);
+%     end
+% end
+% 
+% disp('Press any key to land...')
+% pause;
+% land( ROS_MACE );
+
+% % ============= Test: 3 quad takeoff, kinematic command, and land =========
+% 
+% ROS_MACE.N = 1;
+% %ROS_MACE.operationalAlt = [4 8]; % m
+% %ROS_MACE.agentIDs = [1 2]; % m
+% ROS_MACE.operationalAlt = [2]; % m
+% ROS_MACE.agentIDs = [3]; % SYSID_THISMAV on each quadrotor
 % % warning: only support one quad mission
 % 
 % ROS_MACE.agentIDtoIndex = zeros(1,max(ROS_MACE.agentIDs));
@@ -180,7 +180,7 @@ land( ROS_MACE );
 % % Velocity are in NED frame
 % dynamicTargetRequest = rosmessage(ROS_MACE.kinematicTargetClient);
 % dynamicTargetRequest.Timestamp = rostime('now');
-% dynamicTargetRequest.VehicleID = 1; % Vehicle ID
+% dynamicTargetRequest.VehicleID = ROS_MACE.agentIDs; % Vehicle ID
 % dynamicTargetRequest.CoordinateFrame = 11;
 % dynamicTargetRequest.XP = 0;
 % dynamicTargetRequest.YP = 0;
@@ -207,7 +207,7 @@ land( ROS_MACE );
 % 
 % dynamicTargetRequest = rosmessage(ROS_MACE.kinematicTargetClient);
 % dynamicTargetRequest.Timestamp = rostime('now');
-% dynamicTargetRequest.VehicleID = 1; % Vehicle ID
+% dynamicTargetRequest.VehicleID = ROS_MACE.agentIDs; % Vehicle ID
 % dynamicTargetRequest.CoordinateFrame = 17; % switch to MAV_FRAME_BODY_OFFSET
 % dynamicTargetRequest.XP = 0;
 % dynamicTargetRequest.YP = 0;
@@ -235,7 +235,7 @@ land( ROS_MACE );
 % 
 % dynamicTargetRequest = rosmessage(ROS_MACE.kinematicTargetClient);
 % dynamicTargetRequest.Timestamp = rostime('now');
-% dynamicTargetRequest.VehicleID = 1; % Vehicle ID
+% dynamicTargetRequest.VehicleID = ROS_MACE.agentIDs; % Vehicle ID
 % dynamicTargetRequest.CoordinateFrame = 17; % switch to MAV_FRAME_BODY_OFFSET
 % dynamicTargetRequest.XP = 0;
 % dynamicTargetRequest.YP = 0;
